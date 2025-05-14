a700009f59a892ec2824ec1697ffc7e2a6c63934
Anna Ganisheva <anna.v.ganisheva@gmail.com>
2025-05-14 22:09:19 +0200

    try to fix test

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def load_train_data(path: str = "./data/train.csv") -> pd.DataFrame:
+def load_train_data(path: str = "data/train.csv") -> pd.DataFrame:
     return pd.read_csv(path)
 
-def load_store_data(path: str = "./data/store.csv") -> pd.DataFrame:
+def load_store_data(path: str = "data/store.csv") -> pd.DataFrame:
     return pd.read_csv(path)
 
 def merge_data(train_df: pd.DataFrame, store_df: pd.DataFrame) -> pd.DataFrame:
