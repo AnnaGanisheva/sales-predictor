95cf67c9666015b413795d9060c1e2d61c178b2a
Anna Ganisheva <anna.v.ganisheva@gmail.com>
2025-05-15 08:50:19 +0200

    rewrite test

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,7 +1,20 @@
 from src import data_loader
+import pandas as pd
 
-def test_merge_shapes():
-    train = data_loader.load_train_data()
-    store = data_loader.load_store_data()
+
+
+def test_merge_data_on_fake_data():
+    train = pd.DataFrame({
+        "Store": [1, 2, 3],
+        "Sales": [5000, 6000, 7000]
+    })
+
+    store = pd.DataFrame({
+        "Store": [1, 2, 3],
+        "StoreType": ['a', 'b', 'c']
+    })
+
     merged = data_loader.merge_data(train, store)
+    
     assert merged.shape[0] == train.shape[0]
+    assert "StoreType" in merged.columns
